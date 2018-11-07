651ba5c244b7beb7470442b2c6f3f64fdc1aabfe
Yohei Okabayashi <yohei.kuro48@gmail.com>
2018-11-07 16:27:10 +0900

    hoge

--- train.py
+++ train.py
@@ -6,10 +6,12 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 from torchvision import transforms
+from collections import OrderedDict
+import json
 
 
 def train_pix2pix(models, datasets, optimizers,
-                  num_epochs, batch_size, device, out):
+                  num_epochs, batch_size, device, out, num_workers=2):
     """
     the training function for pix2pix.
 
@@ -18,8 +20,8 @@ def train_pix2pix(models, datasets, optimizers,
     models: dict
         dictionary that contains generator, discriminator
 
-    datasets: torch.utils.data.Dataset
-        dataset of image
+    datasets: dict of torch.utils.data.Dataset
+        dataset of train image, test image
 
     optimizer: dict
         dictionary that contains torch.optim.Optimizer for generator or discriminator
@@ -34,49 +36,51 @@ def train_pix2pix(models, datasets, optimizers,
 
     out: pathlib.Path
         represent output directory
+
+    num_workers: int
+        how many subprocesses to use for data loading.
+         0 means that the data will be loaded in the main process. (default: 2)
     """
-    since = datetime.datetime.now()
     epochs = tqdm(range(num_epochs), desc="Epoch", unit='epoch')
+    phases = ['train', 'val']
     # construct dataloader
-    dataloader = torch.utils.data.DataLoader(datasets, batch_size=batch_size,
-                                             shuffle=True, num_workers=2)
-    dataset_sizes = len(datasets)
-    criterion = nn.BCELoss()
+    dataloader = {phase: torch.utils.data.DataLoader(datasets[phase], batch_size=batch_size,
+                                                     shuffle=(phase == 'train'), num_workers=num_workers)
+                  for phase in ['train', 'val']}
+    dataset_sizes = {phase: len(datasets[phase]) for phase in phases}
     # train loop
+    since = datetime.datetime.now()
+    [for model in models.item]
     for epoch in epochs:
-        iteration = tqdm(dataloader,
-                         desc="Iteration",
-                         unit='iter')
-        epoch_dis_loss = 0.0
-        epoch_gen_loss = 0.0
-        for inputs, outputs in iteration:
-            inputs = inputs.to(device)
-            outputs = outputs.to(device)
-            assert ((inputs > -1) * (inputs < 1)
-                    ).all(), "input data to discriminator range is not from -1 to 1"
-            real_labels = torch.ones(
-                inputs.size()[0], device=device)
-            fake_labels = torch.zeros(
-                inputs.size()[0], device=device)
-            labels = {'real': real_labels, 'fake': fake_labels}
-            ########################################################
-            # (1) Update D network: minimize - 1/ 2 {1/N * log(D(x, y)) + 1/N * log(1 - D(x, G(x, z)))}
-            # minimize - 1/2N * {softplus(-D(x, y)) + softplus(D(x, y))}
-            ######################################################
-            dis_loss = train_dis(models, optimizers['discriminator'],
-                                 inputs, outputs, labels, criterion)
-            #######################################################
-            # (2) Update G netwrk: minimize - 1/N * log(D(x, G(x, z))) + 1/N * lambda * |y - G(x, z)|
-            # minimize - 1/N { softplus(-D(x, G(x, z))) + lambda * |y - G(x, z)|}
-            ######################################################
-            fake_labels = torch.ones_like(
-                fake_labels, device=device)
-            gen_loss = train_gen(
-                models, optimizers['generator'],
-                inputs, fake_labels, criterion)
-            # statistics
-            epoch_dis_loss += dis_loss.item() * inputs.size()[0]
-            epoch_gen_loss += gen_loss.item() * inputs.size()[0]
+        for phase in phases:
+            iteration = tqdm(dataloader,
+                             desc="Iteration",
+                             unit='iter')
+            epoch_dis_loss = 0.0
+            epoch_gen_loss = 0.0
+            for inputs, outputs in iteration:
+                inputs = inputs.to(device)
+                outputs = outputs.to(device)
+                assert ((inputs > -1) * (inputs < 1)
+                        ).all(), "input data to discriminator range is not from -1 to 1"
+                ########################################################
+                # (1) Update D network: minimize - 1/ 2 {1/N * log(D(x, y)) + 1/N * log(1 - D(x, G(x, z)))}
+                # minimize - 1/2N * {softplus(-D(x, y)) + softplus(D(x, y))}
+                ######################################################
+                dis_loss = train_dis(models, optimizers['discriminator'],
+                                     inputs, outputs, labels, criterion)
+                #######################################################
+                # (2) Update G netwrk: minimize - 1/N * log(D(x, G(x, z))) + 1/N * lambda * |y - G(x, z)|
+                # minimize - 1/N { softplus(-D(x, G(x, z))) + lambda * |y - G(x, z)|}
+                ######################################################
+                fake_labels = torch.ones_like(
+                    fake_labels, device=device)
+                gen_loss = train_gen(
+                    models, optimizers['generator'],
+                    inputs, fake_labels, criterion)
+                # statistics
+                epoch_dis_loss += dis_loss.item() * inputs.size()[0]
+                epoch_gen_loss += gen_loss.item() * inputs.size()[0]
 
         # print loss
         epoch_dis_loss /= dataset_sizes
@@ -126,7 +130,7 @@ def train_dis(models, dis_optim, inputs, outputs):
     real = dis(outputs, inputs)
 
     # calc loss for discriminator
-    dis_loss = torch.mean(F.softplus(-real) + F.softplus(fake)) / 2
+    dis_loss = torch.mean(F.softplus(-real) + F.softplus(fake)) * 0.5
 
     # update parameters of discrimminator
     dis_loss.backward()
@@ -135,18 +139,27 @@ def train_dis(models, dis_optim, inputs, outputs):
     return dis_loss
 
 
-def train_gen(models, gen_optim, fake_labels, criterion):
+def train_gen(models, gen_optim, inputs, outputs, lam):
     """
     train generator by a iteration
+
     Parameters
     ---------------
     models: dict
         dictionary that contains generator, discriminator
+
     gen_optim: torch.optim.Optimizer
         optimizer for generator
-    fake_labels: torch.Tensor
-        label coresponding to fake image
-    criterion: torch.BCELoss()
+
+    inputs: torch.Tensor
+        batch data of input image
+
+    outputs: torch.Tensor
+        batch data of output imag
+
+    lam: float
+        cofficient for l1 loss
+
     Return
     --------------
     gen_loss: torch.Tensor
@@ -157,13 +170,12 @@ def train_gen(models, gen_optim, fake_labels, criterion):
     gen.train()
     # zero the parameter gradientsreal
     gen_optim.zero_grad()
-    # make noise & forward
-    z = gen.make_hidden(fake_labels.size()[0]).to(fake_labels.device)
-    x_fake = gen(z)
-    y_fake = dis(x_fake)
+    outputs_fake = gen(inputs)
+    fake = dis(outputs_fake, inputs)
 
     # calc loss for generator
-    gen_loss = criterion(y_fake, fake_labels)
+    gen_loss = torch.mean(F.softplus(-fake)) + lam * \
+        F.l1_loss(outputs_fake, outputs)
 
     # update parameters of discrimminator
     gen_loss.backward()
