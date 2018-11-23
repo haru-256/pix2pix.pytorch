db1d928014f30bbad8a17296f96eb39d57ba094d
Yohei Okabayashi <yohei.kuro48@gmail.com>
2018-11-23 15:16:53 +0900

    ignore assert

--- train_plan.sh
+++ train_plan.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 for seed in $(seq 4 10); do
-    eval "python -0 main.py -n 1 -s $seed -bs 4 --norm_type batch -g 0 -e 15 --dataset edges2shoes --num_workers 4"
+    eval "python -O main.py -n 1 -s $seed -bs 4 --norm_type batch -g 0 -e 15 --dataset edges2shoes --num_workers 4"
 done
