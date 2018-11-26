dc436c5fd4ee223908ced728d0a1fa25fbee87e9
Yohei Okabayashi <yohei.kuro48@gmail.com>
2018-11-26 20:52:17 +0900

    for edges2shoes experiments 2

--- train_plan.sh
+++ train_plan.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-for seed in $(seq 4 10); do
-    eval "python -O main.py -n 1 -s $seed -bs 4 --norm_type batch -g 0 -e 15 --dataset edges2shoes --num_workers 4"
+for seed in $(seq 1 5); do
+    eval "python -O main.py -n 2 -s $seed -bs 1 --norm_type batch -g 0 -e 15 --dataset edges2shoes --num_workers 4"
 done
