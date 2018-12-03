bf42b7c9d065f9567f8685b30dfa194f94e73ba4
Yohei Okabayashi <yohei.kuro48@gmail.com>
2018-12-03 12:20:22 +0900

    for experiments 3

--- train_plan.sh
+++ train_plan.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-for seed in $(seq 3 5); do
-    eval "python -O main.py -n 2 -s $seed -bs 1 --norm_type batch -g 0 -e 15 --dataset edges2shoes --num_workers 4"
+for seed in $(seq 1 5); do
+    eval "python -O main.py -n 3 -s $seed -bs 4 --norm_type instance -g 0 -e 15 --dataset edges2shoes --num_workers 4"
 done
