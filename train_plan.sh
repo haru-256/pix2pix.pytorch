71751f372bd74b4556b4985383f2c61cb0f49e73
Yohei Okabayashi <yohei.kuro48@gmail.com>
2018-12-15 22:18:33 +0900

    hoge

--- train_plan.sh
+++ train_plan.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
 for seed in $(seq 1 5); do
-    eval "python -O main.py -n 5 -s $seed -bs 16 --norm_type batch -g 0 -e 15 --dataset edges2shoes --num_workers 4"
+    eval "python -O main.py -n 6 -s $seed -bs 4 --norm_type instance -g 0 -e 15 --dataset edges2shoes --num_workers 4 --not_affine"
 done
 
