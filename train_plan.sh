5a667dac9e49872be7c773554e13ecf7153444c2
Yohei Okabayashi <yohei.kuro48@gmail.com>
2019-10-07 17:25:38 +0900

    file migration

--- train_plan.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-for seed in $(seq 1 5); do
-    eval "python -O main.py -n 6 -s $seed -bs 4 --norm_type instance -g 0 -e 15 --dataset edges2shoes --num_workers 4 --not_affine"
-done
-
